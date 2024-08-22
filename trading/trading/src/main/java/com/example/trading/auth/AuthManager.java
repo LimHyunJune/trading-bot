a01588ee7f39c7a8fe1580f87e677047d4d244f7
Imu <48524793+LimHyunJune@users.noreply.github.com>
2024-08-22 22:56:52 +0900

    refac: singleton authmanger implement

--- trading/trading/src/main/java/com/example/trading/auth/AuthManager.java
+++ trading/trading/src/main/java/com/example/trading/auth/AuthManager.java
@@ -1,10 +1,67 @@
 package com.example.trading.auth;
 
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
+
+import java.util.Map;
 
 @Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthManager {
-    String authKey;
-    String authSecret;
+    RestClient restClient;
+
+    String appKey;
+    String appSecret;
     String approvalKey;
+
+    @PostConstruct
+    public void init()
+    {
+        restClient = RestClient.create();
+    }
+
+    public AuthManager(String appKey, String appSecret)
+    {
+        this.appKey = appKey;
+        this.appSecret = appSecret;
+    }
+
+    public void getWebSocketConnectionKey()
+    {
+        JSONObject jo = new JSONObject();
+        try{
+            jo.put("grant_type", "client_credentials");
+            jo.put("appkey", appKey);
+            jo.put("secretkey", appSecret);
+        }
+        catch (JSONException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+        try {
+            ResponseEntity<Map> response = restClient.post()
+                    .uri("https://openapi.koreainvestment.com:9443//oauth2/Approval")
+                    .body(jo.toString())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .toEntity(Map.class);
+            this.approvalKey = (String) response.getBody().get("approval_key");
+        }
+        catch (RestClientException e)
+        {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
 }
