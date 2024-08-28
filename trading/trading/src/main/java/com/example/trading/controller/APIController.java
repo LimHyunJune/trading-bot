95ff3e378aa0fb7a3115fd47bbe85565933bcbc2
Imu <48524793+LimHyunJune@users.noreply.github.com>
2024-08-28 23:14:39 +0900

    feat: change APIController register logic

--- trading/trading/src/main/java/com/example/trading/controller/APIController.java
+++ trading/trading/src/main/java/com/example/trading/controller/APIController.java
@@ -1,9 +1,14 @@
 package com.example.trading.controller;
 
-import com.example.trading.auth.AuthManager;
+import com.example.trading.manager.AuthManager;
 import com.example.trading.datafetcher.realtime.RealtimeClosingPriceDataFetcher;
 import com.example.trading.dto.RegisterDTO;
-import com.example.trading.kafka.KafkaManager;
+import com.example.trading.manager.KafkaManager;
+import com.example.trading.model.Buying;
+import com.example.trading.model.Selection;
+import com.example.trading.model.Selling;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,25 +19,66 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class APIController {
 
+    Logger logger = LoggerFactory.getLogger(APIController.class);
+
     @Autowired
     AuthManager authManager;
     @Autowired
     KafkaManager kafkaManager;
     @Autowired
-    RealtimeClosingPriceDataFetcher realtimeClosingPriceDataFetcher;
+    Selection selection;
+    @Autowired
+    Buying buying;
+    @Autowired
+    Selling selling;
 
     @PostMapping("/register")
     public void register(@RequestBody RegisterDTO registerDTO) {
+        logger.info(String.valueOf(registerDTO));
         authManager.setAppKey(registerDTO.getAppKey());
         authManager.setAppSecret(registerDTO.getAppSecret());
-        authManager.getWebSocketConnectionKey();
-        kafkaManager.init(registerDTO.getAnalyticsServer());
+
+        if(registerDTO.getApprovalKey().isEmpty())
+            authManager.setWebSocketConnectionKey();
+        else
+            authManager.setApprovalKey(registerDTO.getApprovalKey());
+
+        if(registerDTO.getAccessToken().isEmpty())
+            authManager.setAuthorizationKey();
+        else
+            authManager.setAccessToken(registerDTO.getAccessToken());
+
+         kafkaManager.init(registerDTO.getAnalyticsServer());
     }
 
-    @GetMapping("/test")
-    public String test()
+    /*
+     * 자동 매매 시 /select, /buy, /sell 통합 가능
+     */
+    @GetMapping("/select")
+    public String select()
     {
-        realtimeClosingPriceDataFetcher.connect("005930");
-        return authManager.getApprovalKey();
+        if(selection.isEmpty())
+            selection.start();
+        return  selection.getFirstStock();
+    }
+
+    @GetMapping("/buy")
+    public void buy()
+    {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/sell")
+    public void sell()
+    {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
