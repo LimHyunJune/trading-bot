45bce58f2592385b245ea8026f5c2e2f68a9f4ed
Imu <48524793+LimHyunJune@users.noreply.github.com>
2024-08-31 19:05:18 +0900

    feat: Buying kafka consumer dependency add

--- trading/trading/src/main/java/com/example/trading/model/Buying.java
+++ trading/trading/src/main/java/com/example/trading/model/Buying.java
@@ -1,7 +1,31 @@
 package com.example.trading.model;
 
+import com.example.trading.datafetcher.realtime.RealtimeClosingPriceDataFetcher;
+import com.example.trading.kafka.consumer.BuyingConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Buying {
+
+    RealtimeClosingPriceDataFetcher realtimeClosingPriceDataFetcher;
+    BuyingConsumer buyingConsumer;
+
+    @Autowired
+    public Buying(RealtimeClosingPriceDataFetcher realtimeClosingPriceDataFetcher, BuyingConsumer buyingConsumer)
+    {
+        this.realtimeClosingPriceDataFetcher = realtimeClosingPriceDataFetcher;
+        this.buyingConsumer = buyingConsumer;
+    }
+
+    public void start(String stock)
+    {
+        realtimeClosingPriceDataFetcher.connect(stock);
+        buyingConsumer.poll();
+    }
+
+    public void complete()
+    {
+        realtimeClosingPriceDataFetcher.disConnect();
+    }
 }
